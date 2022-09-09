1ffd603e51911862631927ea37ccf03058384923
Gilbert Segakweng <shn.shane@gmail.com>
2022-09-09 14:11:08 -0700

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a' ; z--)
+		putchar(z);
+	putchar('\n');
+	return (0);
+}
