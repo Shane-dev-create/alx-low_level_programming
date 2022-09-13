4bfb1c23ae8e65dc41a86a247cf91d9d6e08b343
Gilbert Segakweng <shn.shane@gmail.com>
2022-09-13 15:44:23 -0700

    Print signs based on the value of n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign -> prints sign based on the value of n
  * @n: argument in the function
- *Return: 1, 0 and -1
+ * Return: 1 when n > 0, 0 when n ==0  and -1 when is < 0
  */
 
 int print_sign(int n)
