a87025c269f6b1e39831c9571b4c7f205e3b38f8
Gilbert Segakweng <shn.shane@gmail.com>
2022-09-13 17:03:03 -0700

    Print x9 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,14 @@
 
 void times_table(void)
 {
-	int num, multiplier, prod;
+	int num;
+	int multiplier;
+	int prod;
 
 	for (num = 0; num <= 9; ++num)
 	{
-		_putchar('0');
-		for (multiplier = 1; multiplier <= 9; +multiplier)
+		_putchar('0');i
+		for (multiplier = 1; multiplier <= 9; ++multiplier)
 		{
 			_putchar(',');
 			_putchar(' ');
