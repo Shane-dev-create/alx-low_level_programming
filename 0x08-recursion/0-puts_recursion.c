c5fc31d2d1ffca3a7c6cdcce5515bcfb416ba397
Gilbert Segakweng <shn.shane@gmail.com>
2022-09-28 07:45:44 -0700

    Prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -> print a string followed by a new line
+ * @s:string
+ * Return:void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
