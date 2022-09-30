c16837550d9de1f1b3262fe6623d08d4a8575fc4
Gilbert Segakweng <shn.shane@gmail.com>
2022-09-30 08:23:26 -0700

    Adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * num_check - checks if string contains anything other than number
+ * @s: string to check
+ * Return: boolean
+ */
+int num_check(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * main -> prints the result of adding positive numbers
+ * @argc: argument
+ * @argv: number of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (num_check(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+
+}
