d3f1af4e48c08f0f0cc09040378c73719b40cff9
Gilbert Segakweng <shn.shane@gmail.com>
2022-10-12 13:18:55 -0700

    hotfix basic calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
 /**
- * main -> main function
+ * main - main function
  * @argc: arguments
  * @argv: array of args
- * Return: 0 (for Success)
+ * Return: return something
  */
-
 int main(int argc, char *argv[])
 {
-	int (*function)(int, int);
-
-	if (argc != 4)
+	if (argc == 4)
 	{
-		printf("Error\n");
-		exit(98);
+		int a, b;
+		int (*func)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (0);
 	}
-
-	function = get_op_func(argv[2]);
-
-	if (!function)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", function(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+	printf("Error\n");
+	exit(98);
 }
