084e8bbfa189ea38fb75fa18859b5f9c4c8c7a80
Gilbert Segakweng <shn.shane@gmail.com>
2022-10-12 13:03:13 -0700

    program that performs operarions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * main -> main function
+ * @argc: arguments
+ * @argv: array of args
+ * Return: 0 (for Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	function = get_op_func(argv[2]);
+
+	if (!function)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", function(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
