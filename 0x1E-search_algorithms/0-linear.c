3a1a0a1602f91770af6f0b2b10bd0d47b59b571e
“Gilbert <shn.shane@gmail.com>
2023-04-13 13:03:10 -0700

    hot fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,14 +5,14 @@
  * @array: pointer to first elemnt to search in array
  * @size: number of elements in array
  * @value: value to search for
- * Return: int or -1 if vlue is NULL
+ * Return: int or -1 if value is NULL
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t a = 0;
+	size_t a;
 
-	for (a < size; a++)
+	for (a = 0; a < size; a++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 		if (array[a] == value)
